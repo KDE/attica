9c80c3aea9a452244d2c17a0d25cfa66ea88697c
Frederik Gladhorn <gladhorn@kde.org>
2009-11-04 18:05:54 +0000

    compile ++ (broken comment)
    svn path=/trunk/kdereview/attica/; revision=1044819

--- lib/knowledgebaseentry.h
+++ lib/knowledgebaseentry.h
@@ -18,6 +18,7 @@
 
     You should have received a copy of the GNU Lesser General Public
     License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef ATTICA_KNOWLEDGEBASEENTRY_H
 #define ATTICA_KNOWLEDGEBASEENTRY_H
