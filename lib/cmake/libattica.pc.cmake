2682cbf9554711f2d97675df018ad89d4e463f36
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2013-08-13 15:57:23 +0200

    Add INCLUDE_DESTINATION

--- lib/cmake/libattica.pc.cmake
+++ lib/cmake/libattica.pc.cmake
@@ -1,12 +1,12 @@
 prefix=${CMAKE_INSTALL_PREFIX}
 exec_prefix=${CMAKE_INSTALL_PREFIX}/bin
 libdir=${LIB_DESTINATION}
-includedir=${CMAKE_INSTALL_PREFIX}/include
+includedir=${INCLUDE_DESTINATION}
 
 Name: libattica
 Description: Qt library to access Open Collaboration Services
 #Requires:
 Version: ${CMAKE_LIBATTICA_VERSION_MAJOR}.${CMAKE_LIBATTICA_VERSION_MINOR}.${CMAKE_LIBATTICA_VERSION_PATCH}
 Libs: -L${LIB_DESTINATION} -lattica
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
+Cflags: -I${INCLUDE_DESTINATION}
 
