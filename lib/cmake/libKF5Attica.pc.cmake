c5ee5bf479866dc3e0b790d4f9b9f746579735eb
Jonathan Riddell <jr@jriddell.org>
2014-01-17 16:26:48 +0000

    fix include path will now push change to rename library to libKF5Attica https://git.reviewboard.kde.org/r/114899/

--- lib/cmake/libKF5Attica.pc.cmake
+++ lib/cmake/libKF5Attica.pc.cmake
@@ -8,4 +8,4 @@ Description: Qt library to access Open Collaboration Services
 #Requires:
 Version: ${CMAKE_LIBATTICA_VERSION_MAJOR}.${CMAKE_LIBATTICA_VERSION_MINOR}.${CMAKE_LIBATTICA_VERSION_PATCH}
 Libs: -L${LIB_DESTINATION} -l${ATTICA_LIB_SONAME}
-Cflags: -I${INCLUDE_DESTINATION}
+Cflags: -I${INCLUDE_DESTINATION}/KF5Attica
