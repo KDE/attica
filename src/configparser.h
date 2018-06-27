KDE/attica

commit ebb9d8d9c1a9fe4151507ff94a7e0be6afb317bc

Add provider auto test
The provider is checked by fetching the server config

Test Plan: compiled and tested on linux

Reviewers: leinir

Reviewed By: leinir

Subscribers: kde-frameworks-devel

Tags: #frameworks

Differential Revision: https://phabricator.kde.org/D13733

src/configparser.h
@@ -1,0 +1,43 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2018 Ralf Habacker <ralf.habacker@freenet.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef ATTICA_CONFIGPARSER_H
+#define ATTICA_CONFIGPARSER_H
+
+#include "config.h"
+#include "parser.h"
+
+namespace Attica {
+
+class Config::Parser : public Attica::Parser<Config>
+{
+private:
+    Config parseXml(QXmlStreamReader& xml) override;
+    QStringList xmlElement() const override;
+
+};
+
+}
+
+
+#endif

autotests/CMakeLists.txt
@@ -4,6 +4,8 @@ find_package(Qt5 ${REQUIRED_QT_VERSION} NO_MODULE REQUIRED Test)
 set_package_properties(Qt5Test PROPERTIES PURPOSE "Required for autotests")
 
 ecm_add_tests(
+  configtest.cpp
   persontest.cpp
+  providertest.cpp
   LINK_LIBRARIES Qt5::Test KF5::Attica
 )

autotests/configtest.cpp
@@ -1,0 +1,58 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2018 Ralf Habacker <ralf.habacker@freenet.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <QtTest/QtTest>
+#include <configparser.h>
+
+using namespace Attica;
+
+class ConfigTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testParsing();
+};
+
+void ConfigTest::testParsing()
+{
+    Config::Parser parser;
+    QString validData (QLatin1String("<?xml version=\"1.0\"?>"
+                                     "<ocs><data>"
+                                     "<version>1.7</version>"
+                                     "<website>store.kde.org</website>"
+                                     "<host>api.kde-look.org</host>"
+                                     "<contact>contact@opendesktop.org</contact>"
+                                     "<ssl>true</ssl>"
+                                     "</data></ocs>"));
+    Config config = parser.parse(validData);
+    QVERIFY(config.isValid());
+
+    QString invalidData = QLatin1String("<ocs><braaaaaaaaaaawrlawrf></braaaaaaaaaaawrlawrf></ocs>");
+    config = parser.parse(invalidData);
+    QVERIFY(!config.isValid());
+}
+
+QTEST_MAIN(ConfigTest)
+
+#include "configtest.moc"

autotests/providertest.cpp
@@ -1,0 +1,141 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2018 Ralf Habacker <ralf.habacker@freenet.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <QtTest/QtTest>
+#include <QEventLoop>
+
+#include <config.h>
+#include <providermanager.h>
+
+
+using namespace Attica;
+
+class ProviderTest : public QObject
+{
+    Q_OBJECT
+public:
+    ProviderTest();
+    virtual ~ProviderTest();
+
+private:
+    void initProvider(const QUrl &url);
+
+private Q_SLOTS:
+    void testFetchValidProvider();
+    void testFetchInvalidProvider();
+
+protected Q_SLOTS:
+    void providerAdded(Attica::Provider p);
+    void slotDefaultProvidersLoaded();
+    void slotConfigResult(Attica::BaseJob *j);
+    void slotTimeout();
+
+private:
+    Attica::ProviderManager *m_manager;
+    QEventLoop *m_eventloop;
+    QTimer m_timer;
+};
+
+ProviderTest::ProviderTest()
+  : m_manager(0),
+    m_eventloop(new QEventLoop)
+{
+}
+
+ProviderTest::~ProviderTest()
+{
+    delete m_manager;
+}
+
+void ProviderTest::slotDefaultProvidersLoaded()
+{
+    qDebug() << "default providers loaded";
+    m_eventloop->exit();
+}
+
+void ProviderTest::providerAdded(Attica::Provider p)
+{
+    qDebug() << "got provider" << p.name();
+    m_eventloop->exit();
+}
+
+void ProviderTest::initProvider(const QUrl &url)
+{
+    delete m_manager;
+    m_manager = new Attica::ProviderManager;
+    m_manager->setAuthenticationSuppressed(true);
+    connect(m_manager, SIGNAL(defaultProvidersLoaded()), this, SLOT(slotDefaultProvidersLoaded()));
+    connect(m_manager, SIGNAL(providerAdded(Attica::Provider)), this, SLOT(providerAdded(Attica::Provider)));
+    m_manager->addProviderFile(url);
+    m_timer.singleShot(5000, this, SLOT(slotTimeout()));
+    m_eventloop->exec();
+}
+void ProviderTest::testFetchValidProvider()
+{
+    initProvider(QUrl(QLatin1String("https://autoconfig.kde.org/ocs/providers.xml")));
+    Attica::Provider provider = m_manager->providers().at(0);
+    ItemJob<Config>* job = provider.requestConfig();
+    QVERIFY(job);
+    connect(job, SIGNAL(finished(Attica::BaseJob*)), SLOT(slotConfigResult(Attica::BaseJob*)));
+    job->start();
+    m_eventloop->exec();
+}
+
+void ProviderTest::slotConfigResult(Attica::BaseJob* j)
+{
+    if (j->metadata().error() == Metadata::NoError) {
+        Attica::ItemJob<Config> *itemJob = static_cast<Attica::ItemJob<Config> *>( j );
+        Attica::Config p = itemJob->result();
+        qDebug() << QLatin1String("Config loaded - Server has version") << p.version();
+    } else if (j->metadata().error() == Metadata::OcsError) {
+        qDebug() << QString(QLatin1String("OCS Error: %1")).arg(j->metadata().message());
+    } else if (j->metadata().error() == Metadata::NetworkError) {
+        qDebug() << QString(QLatin1String("Network Error: %1")).arg(j->metadata().message());
+    } else {
+        qDebug() << QString(QLatin1String("Unknown Error: %1")).arg(j->metadata().message());
+    }
+    m_eventloop->exit();
+    m_timer.stop();
+    QVERIFY(j->metadata().error() == Metadata::NoError);
+}
+
+void ProviderTest::slotTimeout()
+{
+    if (m_eventloop->isRunning()) {
+        m_eventloop->exit();
+        QFAIL("Could not fetch provider");
+    }
+}
+
+void ProviderTest::testFetchInvalidProvider()
+{
+    // TODO error state could only be checked indirectly by timeout
+    initProvider(QUrl(QLatin1String("https://invalid-url.org/ocs/providers.xml")));
+    m_timer.singleShot(5000, this, SLOT(slotTimeout()));
+    m_eventloop->exec();
+    QVERIFY(m_manager->providers().size() == 0);
+}
+
+QTEST_MAIN(ProviderTest)
+
+#include "providertest.moc"

src/CMakeLists.txt
@@ -26,6 +26,8 @@ set(attica_SRCS
    categoryparser.cpp
    comment.cpp
    commentparser.cpp
+   config.cpp
+   configparser.cpp
    content.cpp
    contentparser.cpp
    deletejob.cpp
@@ -102,6 +104,7 @@ ecm_generate_headers(Attica_CamelCase_HEADERS
   AccountBalance
   Achievement
   Comment
+  Config
   Content
   Category
   DeleteJob

src/config.cpp
@@ -1,0 +1,116 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2018 Ralf Habacker <ralf.habacker@freenet.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "config.h"
+
+using namespace Attica;
+
+class Config::Private : public QSharedData {
+    public:
+        QString m_version;
+        QString m_website;
+        QString m_host;
+        QString m_contact;
+        bool m_ssl;
+
+        Private()
+            : m_ssl(false)
+        {
+        }
+};
+
+
+Config::Config()
+    : d(new Private)
+{
+}
+
+Config::Config(const Attica::Config& other)
+    : d(other.d)
+{
+}
+
+Config& Config::operator=(const Attica::Config & other)
+{
+    d = other.d;
+    return *this;
+}
+
+Config::~Config()
+{
+}
+
+QString Attica::Config::version() const
+{
+    return d->m_version;
+}
+
+QString Config::website() const
+{
+    return d->m_website;
+}
+
+QString Config::host() const
+{
+    return d->m_host;
+}
+
+QString Config::contact() const
+{
+    return d->m_contact;
+}
+
+bool Config::ssl() const
+{
+    return d->m_ssl;
+}
+
+bool Config::isValid() const
+{
+  return !(d->m_version.isEmpty());
+}
+
+void Config::setContact(const QString &contact)
+{
+    d->m_contact = contact;
+}
+
+void Config::setVersion(const QString &version)
+{
+    d->m_version = version;
+}
+
+void Config::setWebsite(const QString &website)
+{
+    d->m_website = website;
+}
+
+void Config::setHost(const QString &host)
+{
+    d->m_host = host;
+}
+
+void Config::setSsl(bool ssl)
+{
+    d->m_ssl = ssl;
+}

src/config.h
@@ -1,0 +1,90 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2018 Ralf Habacker <ralf.habacker@freenet.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+#ifndef ATTICA_CONFIG_H
+#define ATTICA_CONFIG_H
+
+#include <QtCore/QString>
+#include <QtCore/QSharedDataPointer>
+
+#include "attica_export.h"
+
+namespace Attica {
+
+/**
+ * Represents a server config
+ */
+class ATTICA_EXPORT Config
+{
+  public:
+    typedef QList<Config> List;
+    class Parser;
+
+    /**
+     * Creates an empty Config
+     */
+    Config();
+
+    /**
+     * Copy constructor.
+     * @param other the Config to copy from
+     */
+    Config(const Config& other);
+
+    /**
+     * Assignment operator.
+     * @param other the Config to assign from
+     * @return pointer to this Config
+     */
+    Config& operator=(const Config& other);
+
+    /**
+     * Destructor.
+     */
+    ~Config();
+
+    QString contact() const;
+    QString host() const;
+    QString version() const;
+    bool ssl() const;
+    QString website() const;
+
+    void setContact(const QString &contact);
+    void setHost(const QString &host);
+    void setSsl(bool ssl);
+    void setVersion(const QString &version);
+    void setWebsite(const QString &website);
+
+    /**
+     * Checks whether this config is valid
+     * @return @c true if config is valid, @c false otherwise
+     */
+    bool isValid() const;
+
+  private:
+    class Private;
+    QSharedDataPointer<Private> d;
+};
+
+}
+
+#endif

src/configparser.cpp
@@ -1,0 +1,61 @@
+/*
+    This file is part of KDE.
+
+    Copyright (c) 2018 Ralf Habacker <ralf.habacker@freenet.de>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) version 3, or any
+    later version accepted by the membership of KDE e.V. (or its
+    successor approved by the membership of KDE e.V.), which shall
+    act as a proxy defined in Section 6 of version 3 of the license.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "configparser.h"
+
+
+using namespace Attica;
+
+Config Config::Parser::parseXml(QXmlStreamReader& xml)
+{
+    Config config;
+
+    while (!xml.atEnd()) {
+        xml.readNext();
+
+        if (xml.isStartElement()) {
+            if (xml.name() == QLatin1String("version")) {
+                config.setVersion( xml.readElementText());
+            } else if (xml.name() == QLatin1String("website")) {
+                config.setWebsite( xml.readElementText());
+            } else if (xml.name() == QLatin1String("host")) {
+                config.setHost( xml.readElementText());
+            } else if (xml.name() == QLatin1String("contact")) {
+                config.setContact( xml.readElementText());
+            } else if (xml.name() == QLatin1String("ssl")) {
+                config.setSsl(xml.readElementText() == QLatin1String("true"));
+            }
+        }
+
+        if (xml.isEndElement() && xml.name() == QLatin1String("data")) {
+            break;
+        }
+    }
+
+    return config;
+}
+
+
+QStringList Config::Parser::xmlElement() const {
+    return QStringList(QLatin1String( "data" ));
+}

commit b173d478076ea89ddc4109d04d8252fa207735f7

Unbreak building tests on Windows
Caused by ebb9d8d9c1a9

src/configparser.h
@@ -27,9 +27,11 @@
 #include "config.h"
 #include "parser.h"
 
+#include "attica_export.h"
+
 namespace Attica {
 
-class Config::Parser : public Attica::Parser<Config>
+class ATTICA_EXPORT Config::Parser : public Attica::Parser<Config>
 {
 private:
     Config parseXml(QXmlStreamReader& xml) override;