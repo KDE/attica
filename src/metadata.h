028b4c1e826f3ae74c0e6ac54e0167dfd0e035bb
Dan Leinir Turthra Jensen <admin@leinir.dk>
2021-06-03 10:33:24 +0100

    Do the documentation thing, with @since and whatnot

--- src/metadata.h
+++ src/metadata.h
@@ -74,8 +74,21 @@ public:
     QString resultingId();
     void setResultingId(const QString &id);
 
-    /// The http headers for the most recent network action in the case of a network error
+    /**
+     * The http headers for the most recent network action in the case of a network error
+     * Use this to further inspect the error condition if the OCS status code and string is
+     * not enough to work out precisely what has happened (for example in case of a HTTP
+     * 503 status, which would suggest the service is down for maintenance for an expected
+     * duration which might be read from the Retry-After header).
+     * @return The list of raw headers (equivalent to a QNetworkReply::rawHeaderPairs call)
+     * @since 5.83
+     */
     QList<QNetworkReply::RawHeaderPair> headers() const;
+    /**
+     * Sets the http headers read by headers()
+     * @param headers The new list of raw headers
+     * @since 5.83
+     */
     void setHeaders(const QList<QNetworkReply::RawHeaderPair> &headers);
 
 private:
